2a8bf8bc1e21cda295da13d2e30dc6516ded243c
cory <cbencker@nd.gov>
2026-06-17 16:23:13 -0500

    Update Snacks picker settings

--- .config/nvim/lua/plugins/snacks.lua
+++ .config/nvim/lua/plugins/snacks.lua
@@ -11,6 +11,75 @@ return {
     picker = {
       -- Show hidden files in file search pickers
       hidden = true,
+      sources = {
+        files = {
+          hidden = true,
+          ignored = false,
+          exclude = {
+            "**/.git",
+            "**/.cfg", -- Personal dotfiles git folder
+
+            -- Windows
+            "desktop.ini",
+            "ntuser.*",
+            "NTUSER.*",
+            "*.lnk",
+
+            -- Office
+            "*.doc",
+            "*.docx",
+            "*.xls",
+            "*.xlsx",
+            "*.ppt",
+            "*.pptx",
+            "*.pdf",
+
+            -- Binaries
+            "*.o",
+            "*.so",
+            "*.a",
+            "*.dll",
+            "*.exe",
+            "*.bin",
+            "*.dat",
+            "*.pyc",
+            "*.class",
+            "*.jar",
+            "*.ttf",
+
+            -- Media
+            "*.png",
+            "*.jpg",
+            "*.jpeg",
+            "*.gif",
+            "*.webp",
+            "*.mp3",
+            "*.mp4",
+            "*.mov",
+            "*.avi",
+
+            -- Archives
+            "*.zip",
+            "*.tar",
+            "*.gz",
+            "*.xz",
+            "*.7z",
+            "**/.git/*",
+
+            -- Build artifacts
+            "**/node_modules",
+            "**/dist",
+            "**/build",
+            "*.o",
+            "*.so",
+            "*.a",
+            "*.exe",
+            "*.dll",
+            "*.pyc",
+            "*.class",
+          },
+        },
+      },
     },
     explorer = {
       files = {
