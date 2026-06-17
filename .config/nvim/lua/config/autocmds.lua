b07d17220f42f4c13e5dcdef4639646c38806119
cory <cbencker@nd.gov>
2026-06-17 16:32:28 -0500

    Fix Markdown fold loading

--- .config/nvim/lua/config/autocmds.lua
+++ .config/nvim/lua/config/autocmds.lua
@@ -6,3 +6,13 @@
 --
 -- Or remove existing autocmds by their group name (which is prefixed with `lazyvim_` for the defaults)
 -- e.g. vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")
+
+-- Make sure to restore Markdown folds after loading a Markdown file
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = "markdown",
+  callback = function()
+    vim.schedule(function()
+      pcall(vim.cmd, "loadview")
+    end)
+  end,
+})
