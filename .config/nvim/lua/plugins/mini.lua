3a856d627d031a04009a484543692621d0aad29a
cory <cbencker@nd.gov>
2026-07-16 11:35:46 -0500

    Add mini.surround

--- .config/nvim/lua/plugins/mini.lua
+++ .config/nvim/lua/plugins/mini.lua
@@ -1,5 +1,20 @@
 return {
     {
+        "nvim-mini/mini.surround",
+        version = false,
+        opts = {
+            mappings = {
+                add = "gsa", -- Add surrounding in Normal and Visual modes
+                delete = "gsd", -- Delete surrounding
+                find = "gsf", -- Find surrounding (to the right)
+                find_left = "gsF", -- Find surrounding (to the left)
+                highlight = "gsh", -- Highlight surrounding
+                replace = "gsr", -- Replace surrounding
+                update_n_lines = "gsn", -- Update `n_lines`
+            },
+        },
+    },
+    {
         "nvim-mini/mini.trailspace",
         event = "VeryLazy",
         config = function()
